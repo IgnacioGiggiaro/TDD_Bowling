643ceabda460d31c23c7756161dc6acb35348a94
nacho <nachogiggiaro2000@gmail.com>
2023-05-22 21:57:56 -0300

    score

--- Tdd_bowling.js
+++ Tdd_bowling.js
@@ -1,15 +1,40 @@
 class Juego {
     constructor() {
-        this.puntuacionTotal = 0;
+        this.rolls = [];
     }
 
     tirar(pinos) {
-        // No necesitamos hacer nada en este caso, ya que el jugador no derriba ningún pin
+        this.rolls.push(pinos);
     }
 
     puntuacion() {
-        return this.puntuacionTotal;
+        let score = 0;
+        let rollIndex = 0;
+        const rolls = this.rolls;
+
+        for (let frame = 0; frame < 10; frame++) {
+            if (isSpare()) {
+                score += 10 + spareBonus();
+                rollIndex += 2;
+            } else {
+                score += sumOfBallsInFrame();
+                rollIndex += 2;
+            }
+        }
+        return score;
+
+        function isSpare() {
+            return rolls[rollIndex] + rolls[rollIndex + 1] === 10;
+        }
+        function spareBonus() {
+            return rolls[rollIndex + 2];
+        }
+
+        function sumOfBallsInFrame() {
+            return rolls[rollIndex] + rolls[rollIndex + 1];
+        }
     }
+
 }
 
 module.exports = Juego;
--- bowling.test.js
+++ bowling.test.js
@@ -1,4 +1,3 @@
-// Importar la clase Juego
 const Juego = require('./Tdd_bowling');
 
 // Prueba para un juego de perdedor
@@ -11,4 +10,28 @@ test('Juego de perdedor', () => {
 
     const puntuacion = juego.puntuacion();
     expect(puntuacion).toBe(0);
+});
+
+// Prueba para un juego de todos 1
+test('Juego de todos 1', () => {
+    const juego = new Juego();
+
+    for (let tiro = 0; tiro < 20; tiro++) {
+        juego.tirar(1); // El jugador derriba 1 pin en cada tiro
+    }
+
+    const puntuacion = juego.puntuacion();
+    expect(puntuacion).toBe(20);
+});
+
+test('Juego con al menos un Spare', () => {
+    const juego = new Juego();
+
+    juego.tirar(5); // Frame 1, lanzamiento 1: Se derriban 5 pinos
+    juego.tirar(5); // Frame 1, lanzamiento 2: Se derriban 5 pinos (Spare)
+    juego.tirar(3); // Frame 2, lanzamiento 1: Se derriban 3 pinos
+    juego.tirar(5);
+
+    const puntuacion = juego.puntuacion();
+    expect(puntuacion).toBe(21);
 });
